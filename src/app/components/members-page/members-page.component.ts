7cbe81fdecff0ccfe62a878b715393d0d5e5d912
Jodaias Barreto da Silva <jodaias.silva@iatec.com>
2024-07-23 00:52:51 -0300

    Alterando injeção de dependencia para pegar o user em members-page

--- src/app/components/members-page/members-page.component.ts
+++ src/app/components/members-page/members-page.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { OKTA_AUTH } from '@okta/okta-angular';
-import OktaAuth from '@okta/okta-auth-js';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
     selector: 'app-members-page',
@@ -10,10 +9,11 @@ import OktaAuth from '@okta/okta-auth-js';
 })
 export class MembersPageComponent implements OnInit {
   user: string = '';
-  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) {}
+  constructor(private auth: AuthService) {}
 
   async ngOnInit() {
-    const user = await this.oktaAuth.getUser();
-    this.user = JSON.stringify(user, null, 4);
+    this.auth.user$.subscribe((res: any) => {
+      this.user = JSON.stringify(res, null, 4);
+    });
   }
 }
